3517857e040117aa24ca3bf69f01326faadb260e
Nemuru <skater@boss.staszic.waw.pl>
2012-10-31 17:58:37 +0100

    Create Graph.hpp

---
+++ trunk/Graph.hpp
@@ -0,0 +1,22 @@
+#ifndef GRAPH
+#define GRAPH
+#include "headers.hpp"
+namespace mat
+{
+	virtual class BaseGraph
+	{
+		public:
+		virtual BaseGraph(int nodes);
+		virtual ~BaseGraph();
+		//virtual
+		/*
+		 * metody:
+		 * konstruktor...
+		 * 
+		 */
+		
+	};
+
+}
+#endif
+
